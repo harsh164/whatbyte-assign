f83426cc5414ff4e4d5cb9b566af1a28d7f9eb55
harsh164 <155755263+harsh164@users.noreply.github.com>
2026-01-08 16:16:49 +0530

    Register Doctor model with custom admin options
    Refactor Doctor registration in admin panel to use a custom ModelAdmin class with list display and search fields.

--- core/doctors/admin.py
+++ core/doctors/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Doctor
 
-admin.site.register(Doctor)
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "specialization",
+    )
+    search_fields = (
+        "name",
+        "specialization",
+    )
