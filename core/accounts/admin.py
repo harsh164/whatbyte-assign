5c2f7811bd88b7faf33a76fd98830b5d4ae03097
harsh164 <155755263+harsh164@users.noreply.github.com>
2026-01-08 16:16:17 +0530

    Register User model with custom admin interface
    Added UserAdmin class to customize User model admin interface.

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "is_staff",
+        "is_active",
+        "date_joined",
+    )
+    list_filter = (
+        "is_staff",
+        "is_active",
+    )
+    search_fields = (
+        "email",
+    )
+    ordering = (
+        "-date_joined",
+    )
