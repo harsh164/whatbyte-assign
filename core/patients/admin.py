1afd509ad6cd5114ebcd0bb474be4ae0d4feb1b6
harsh164 <155755263+harsh164@users.noreply.github.com>
2026-01-08 16:17:18 +0530

    Update admin.py

--- core/patients/admin.py
+++ core/patients/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import Patient, PatientDoctor
 
-admin.site.register(Patient)
-admin.site.register(PatientDoctor)
+
+class PatientDoctorInline(admin.TabularInline):
+    model = PatientDoctor
+    extra = 0
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "age",
+    )
+    search_fields = (
+        "name",
+    )
+    inlines = [PatientDoctorInline]
+
+
+@admin.register(PatientDoctor)
+class PatientDoctorAdmin(admin.ModelAdmin):
+    list_display = (
+        "patient",
+        "doctor",
+    )
